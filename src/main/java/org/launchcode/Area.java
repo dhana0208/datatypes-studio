d8044b4287253a7098e3b62565ecc6fec94e0f19
dhana0208 <techdhana33@gmail.com>
2023-08-22 17:38:25 -0500

    studio changes

--- src/main/java/org/launchcode/Area.java
+++ src/main/java/org/launchcode/Area.java
@@ -8,8 +8,18 @@ public class Area {
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter a radius:");
-        Double radius = sc.nextDouble();
-        Double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius  "+ radius +" is : "+area);
+        String radius = sc.next();
+        try {
+            while (Double.parseDouble(radius) < 0) {
+                System.out.println(radius + " is not a valid value");
+                System.out.println("Enter a radius:");
+                radius = sc.next();
+            }
+            Double area = Circle.getArea(Double.valueOf(radius));
+            System.out.println("The area of a circle of radius  " + radius + " is : " + area);
+        }catch (NumberFormatException e){
+            System.out.println(radius +" entered is a string which is not a valid number");
+            System.exit(1);
+        }
     }
 }
